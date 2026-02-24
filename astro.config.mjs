3d363fa8e69a9951ff956641278461304307cf24
paulcartagena <paulcartagena04@gmail.com>
2026-02-24 10:47:11 -0600

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://egmechanic.com',
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()]
 });
